cbbd78f512559dbfd15baa9e26aa87dcbeaf0bd8
Roy van de Water <dev+roy@integrumtech.com>
2014-10-16 12:03:52 -0700

    Add OS specific download links

--- js/script.js
+++ js/script.js
@@ -7,25 +7,12 @@ $(document).ready(function(){
   $('.promo-03').parallax();
   $('.promo-04').parallax();
 
-  /* nice scroll */
-  $( 'html' ).niceScroll({
-    cursorcolor: '#5BBF21',
-    cursorwidth: '10px',
-    cursorborder: '1px solid #5BBF21',
-    cursoropacitymax: 0.9,                
-    scrollspeed: 120,
-    autohidemode: false,
-    horizrailenabled: false,
-    cursorborderradius: 2,
-    zindex: 1060
-  });
-
   /* scrolltop */
   $('.navbar-nav li a, .menus').on('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
         scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
+    }, 500, 'easeInOutExpo');
     event.preventDefault();
   });
 
@@ -43,13 +30,13 @@ $(document).ready(function(){
   // $("#map").gmap3({
   //   map: {
   //       options: {
-  //         center: [-7.866315,110.389574], 
+  //         center: [-7.866315,110.389574],
   //         zoom: 12,
   //         scrollwheel: false
-  //       }  
+  //       }
   //    },
   //   marker:{
-  //       latLng: [-7.866315,110.389574], 
+  //       latLng: [-7.866315,110.389574],
   //       options: {
   //        icon: new google.maps.MarkerImage(
   //          "https://dl.dropboxusercontent.com/u/29545616/Preview/location.png",
